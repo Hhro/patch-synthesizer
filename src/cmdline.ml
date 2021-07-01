c44ad32f47e73aa5cbaa0756ecf55a1a21d76b24
Kihong Heo <kihong.heo@kaist.ac.kr>
2021-07-01 23:24:57 +0900

    init

---
+++ src/cmdline.ml
@@ -0,0 +1,9 @@
+let work_dir : string option ref = ref None
+
+let out_dir = ref "synthesizer-out"
+
+let options = [ ("-outdir", Arg.Set_string out_dir, "") ]
+
+let parse_arg x =
+  work_dir := Some x;
+  ()
---
+++ src/main.ml
@@ -0,0 +1,23 @@
+module F = Format
+
+let initialize work_dir =
+  let out_dir = Filename.concat work_dir !Cmdline.out_dir in
+  (try Unix.mkdir out_dir 0o775 with Unix.Unix_error (Unix.EEXIST, _, _) -> ());
+  print_endline ("Logging to " ^ out_dir);
+  Logging.log_file :=
+    Filename.concat out_dir "log.txt" |> open_out |> Option.some;
+  Logging.log_formatter :=
+    Option.map F.formatter_of_out_channel !Logging.log_file
+
+let main () =
+  let usageMsg = "Usage: synthesizer [options] [work dir]" in
+  Arg.parse Cmdline.options Cmdline.parse_arg usageMsg;
+  match !Cmdline.work_dir with
+  | None ->
+      prerr_endline "Error: No work directory is given";
+      exit 1
+  | Some work_dir ->
+      initialize work_dir;
+      ()
+
+let _ = main ()
